040536a11038f3d1921ded92e74c7c4844923624
thomas <thomas.wolfertstetter@web.de>
2018-05-19 10:54:00 +0200

    added gui

---
+++ src/GUI/Controller.java
@@ -0,0 +1,58 @@
+package GUI;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import application.IFacebookUploader;
+import application.impl.FacebookUploader;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+
+public class Controller implements Initializable {
+
+	private IFacebookUploader uploader;
+	
+	@FXML
+	private TextField accesstoken;
+	@FXML
+	private TextField pageID;
+	@FXML
+	private Text loginNotification;
+	@FXML
+	private TextField intervall;
+	@FXML
+	private TextField path;	
+	@FXML
+	private Text searchNotification;
+		
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		uploader = new FacebookUploader();
+	}
+	
+	public void login() {
+		if(uploader.login(accesstoken.getText(), pageID.getText())) {
+			loginNotification.setText("login erfolgreich");
+			loginNotification.setFill(Color.GREEN);
+		} else {
+			loginNotification.setText("login fehlgeschlagen");
+			loginNotification.setFill(Color.RED);
+		}
+	}
+	
+	public void start() {
+		try {
+			uploader.start(Integer.parseInt(intervall.getText()) * 1000, path.getText());
+			searchNotification.setText("");
+		} catch(NumberFormatException e){
+			searchNotification.setText("bitte Ganzzahl eingeben");
+		}
+	}
+	
+	public void stop() {
+		uploader.stop();
+	}
+}
